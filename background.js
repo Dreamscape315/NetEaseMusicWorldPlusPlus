fa8e30406b1a2c0badee3b859940904793a9d42d
Akatsuki Sky <45032480+Dreamscape315@users.noreply.github.com>
2025-03-14 13:49:42 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,39 +1,79 @@
-let mode
-const title = ['closed', 'normal', 'enhanced']
-const icon = ['images/grey.svg', 'images/red.svg', 'images/blue.svg']
+let mode;
+const title = ['closed', 'normal', 'enhanced'];
+const icon = [
+	{ 16: 'images/logo16.png', 48: 'images/logo48.png', 128: 'images/logo128.png' }, // grey
+	{ 16: 'images/logo16.png', 48: 'images/logo48.png', 128: 'images/logo128.png' }, // red
+	{ 16: 'images/logo16.png', 48: 'images/logo48.png', 128: 'images/logo128.png' }  // blue
+];
+//const icon = ['images/grey.svg', 'images/red.svg', 'images/blue.svg'];
 
-chrome.webRequest.onBeforeSendHeaders.addListener(
-	details => {
-		const { url, requestHeaders } = details
-		if (url.includes('music.163.com')) {
-			if (mode > 0) requestHeaders.push({
-				name: 'X-Real-IP',
-				value: '211.161.244.70'
-			})
-		} else if (url.includes('music.126.net')) {
-			if (/m\d+c/.test(url)) requestHeaders.push({
-				name: 'Cache-Control',
-				value: 'no-cache'
-			})
+
+const createRules = () => {
+	chrome.storage.local.get('mode', data => {
+		mode = data.mode == null ? 2 : data.mode;
+		updateRules();
+		sync();
+	});
+};
+
+const updateRules = () => {
+	chrome.declarativeNetRequest.updateDynamicRules({
+		removeRuleIds: [1, 2] 
+	}).then(() => {
+		if (mode > 0) {
+			const rules = [
+				{
+					id: 1,
+					priority: 1,
+					action: {
+						type: 'modifyHeaders',
+						requestHeaders: [
+							{ header: 'X-Real-IP', operation: 'set', value: '211.161.244.70' }
+						]
+					},
+					condition: {
+						urlFilter: '||music.163.com',
+						resourceTypes: ['xmlhttprequest', 'main_frame', 'sub_frame', 'script', 'other']
+					}
+				},
+				{
+					id: 2,
+					priority: 1,
+					action: {
+						type: 'modifyHeaders',
+						requestHeaders: [
+							{ header: 'Cache-Control', operation: 'set', value: 'no-cache' }
+						]
+					},
+					condition: {
+						urlFilter: '*m*c*||music.126.net',
+						resourceTypes: ['xmlhttprequest', 'main_frame', 'sub_frame', 'script', 'image', 'other']
+					}
+				}
+			];
+
+			chrome.declarativeNetRequest.updateDynamicRules({
+				addRules: rules
+			});
 		}
-		return { requestHeaders }
-	},
-	{ urls: ['*://music.163.com/*', '*://*.music.126.net/*'] },
-	['blocking', 'requestHeaders']
-)
+	});
+};
 
 const sync = () => {
-	chrome.storage.local.set({ mode })
-	chrome.browserAction.setIcon({ path: icon[mode] })
-	chrome.browserAction.setTitle({ title: `${chrome.i18n.getMessage('name')} [${chrome.i18n.getMessage(title[mode])}]` })
-}
+	chrome.storage.local.set({ mode });
+	chrome.action.setIcon({ path: icon[mode] });
+	chrome.action.setTitle({ title: `${chrome.i18n.getMessage('name')} [${chrome.i18n.getMessage(title[mode])}]` });
+};
 
-chrome.browserAction.onClicked.addListener(() => {
-	mode = (mode + 1) % 3
-	sync()
-})
+chrome.action.onClicked.addListener(() => {
+	mode = (mode + 1) % 3;
+	sync();
+	updateRules(); 
+});
 
-chrome.storage.local.get('mode', data => {
-	mode = data.mode == null ? 2 : data.mode
-	sync()
-})
+createRules();
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	sendResponse({});
+	return true;
+});
